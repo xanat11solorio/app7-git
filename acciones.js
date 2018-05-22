e3c1df2d73bf6d61a2abce2a45e3d6907f74f0ae
xanat11solorio <38302293+xanat11solorio@users.noreply.github.com>
2018-05-22 14:19:25 -0500

    3SUBIDA

--- acciones.js
+++ acciones.js
@@ -1,23 +1,23 @@
 // JavaScript Document
 
 $(document).ready(function(e) {
-	document.addEventListener("deviceready",function(){
-		$('#izquierda').on("swipeleft", function(){
-			navigator.notification.alert("deslizó hacia la izquierda",function() {"aplicacion 7", "ok"});
-		});
-		$('#derecha').on("swiperight", function(){
-			navigator.notification.alert("¿Que quieres hacer?",function(op) {
+    document.addEventListener("deviceready",function(){
+	$('#izquierda').on("swipeleft", function(){
+		navigator.notification.alert("Deslizó hacia la izquierda", function(){"aplicacion7","Ok"});
+		});//cerrar deslizar hacia la izquierda
+		$('#derecha').on("swiperight",function(){
+			navigator.notification.confirm("¿Qué quieres hacer?",function(op){
 				switch(op)
 				{
 					case 1:
 					navigator.notification.beep(1);
 					break;
-					
 					case 2:
 					navigator.notification.vibrate(3000);
 					break;
-				}
-			},"aplicacion 7", "Beepear,Vibrar,Cancelar");
-		}); //cerrar deslizar derecha
-	},false); //cerrar deviceready		
-}); // cerrar document
+					
+				 }
+			},"aplicacion 7","beepear,vibrate,cancelar");
+		});//cerrar deslizar hacia derecha
+	},false);//cerrer deviceready
+});//cerrar document
